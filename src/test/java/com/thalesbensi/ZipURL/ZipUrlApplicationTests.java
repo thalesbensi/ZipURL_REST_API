a61d9de97e2a8eeeab03a76994ca006ef8bd8e9c
thalesbensi <thalesbensi@outlook.com>
2025-06-26 22:06:32 -0300

    Project Setup

---
+++ src/test/java/com/thalesbensi/ZipURL/ZipUrlApplicationTests.java
@@ -0,0 +1,13 @@
+package com.thalesbensi.ZipURL;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZipUrlApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
