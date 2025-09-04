12ec92088bff9ed6f3524d45036ac9c49c37d2eb
thalesbensi <thalesbensi@outlook.com>
2025-09-03 21:44:36 -0300

    feat: ShortUrl Controller implemented

---
+++ src/main/java/com/thalesbensi/ZipURL/api/controllers/ShortUrlController.java
@@ -0,0 +1,9 @@
+package com.thalesbensi.ZipURL.api.controllers;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/shortMyUrl")
+public class ShortUrlController {
+}
