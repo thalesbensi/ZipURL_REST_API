d735ae3537ef7e8b07f6516d30f0e8e145334b61
thalesbensi <thalesbensi@outlook.com>
2025-09-10 20:41:02 -0300

    chore: Remove unnecessary blank line in ShortURL model

--- src/main/java/com/thalesbensi/ZipURL/domain/models/ShortURL.java
+++ src/main/java/com/thalesbensi/ZipURL/domain/models/ShortURL.java
@@ -1,6 +1,5 @@
 package com.thalesbensi.ZipURL.domain.models;
 
-
 import lombok.Data;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
