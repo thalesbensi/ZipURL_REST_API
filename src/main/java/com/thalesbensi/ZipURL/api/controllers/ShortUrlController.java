7e3d8495ceac9e3e01fad9a0063b8c68ae6fe794
thalesbensi <thalesbensi@outlook.com>
2025-09-10 18:48:16 -0300

    feat: Refactor ShortUrlController to use ShortUrlService and update request mappings

--- src/main/java/com/thalesbensi/ZipURL/api/controllers/ShortUrlController.java
+++ src/main/java/com/thalesbensi/ZipURL/api/controllers/ShortUrlController.java
@@ -1,9 +1,37 @@
 package com.thalesbensi.ZipURL.api.controllers;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.thalesbensi.ZipURL.domain.services.ShortUrlService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("/shortMyUrl")
+@RequestMapping("/shorturl")
 public class ShortUrlController {
+
+    final ShortUrlService shortUrlService;
+
+    public ShortUrlController(ShortUrlService shortUrlService) {
+        this.shortUrlService = shortUrlService;
+    }
+
+    @GetMapping("/{shortCode}")
+    public ResponseEntity<Void> redirectToOriginalUrl(@PathVariable String shortCode) {
+        String originalUrl = shortUrlService.getOriginalUrl(shortCode);
+        if (originalUrl != null) {
+            return ResponseEntity.status(HttpStatus.FOUND)
+                    .header("Location", originalUrl)
+                    .build();
+        } else {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
+        }
+    }
+
+    @PostMapping()
+    public ResponseEntity<String> createShortUrl(@RequestBody String originalUrl) {
+        String shortedUrl = shortUrlService.createShortUrl(originalUrl);
+        return ResponseEntity.status(HttpStatus.CREATED)
+                .body(shortedUrl);
+    }
+
 }
