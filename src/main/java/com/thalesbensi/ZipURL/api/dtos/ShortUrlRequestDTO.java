576ef55fa2772a86c8a251ad8c7db9f589e8e970
thalesbensi <thalesbensi@outlook.com>
2025-09-16 21:27:53 -0300

    feat: Add validation annotations to ShortUrlRequestDTO for URL input

--- src/main/java/com/thalesbensi/ZipURL/api/dtos/ShortUrlRequestDTO.java
+++ src/main/java/com/thalesbensi/ZipURL/api/dtos/ShortUrlRequestDTO.java
@@ -1,4 +1,10 @@
 package com.thalesbensi.ZipURL.api.dtos;
 
-public record ShortUrlRequestDTO(String originalUrl) {
-}
+import org.hibernate.validator.constraints.URL;
+import jakarta.validation.constraints.NotBlank;
+
+public record ShortUrlRequestDTO(
+        @NotBlank(message = "The URL can't be blank")
+        @URL(message = "Invalid URL")
+        String originalUrl
+) {}
