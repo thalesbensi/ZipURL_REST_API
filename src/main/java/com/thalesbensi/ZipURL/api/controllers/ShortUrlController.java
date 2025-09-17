51280e5ead772e7f0e95b2989174153f70dd4d70
thalesbensi <thalesbensi@outlook.com>
2025-09-16 21:28:02 -0300

    feat: Add validation to createShortUrl method in ShortUrlController

--- src/main/java/com/thalesbensi/ZipURL/api/controllers/ShortUrlController.java
+++ src/main/java/com/thalesbensi/ZipURL/api/controllers/ShortUrlController.java
@@ -3,6 +3,7 @@ package com.thalesbensi.ZipURL.api.controllers;
 import com.thalesbensi.ZipURL.api.dtos.ShortUrlRequestDTO;
 import com.thalesbensi.ZipURL.api.dtos.ShortUrlResponseDTO;
 import com.thalesbensi.ZipURL.domain.services.ShortUrlService;
+import jakarta.validation.Valid;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -37,7 +38,7 @@ public class ShortUrlController {
     }
 
     @PostMapping
-    public ResponseEntity<String> createShortUrl(@RequestBody ShortUrlRequestDTO shortUrlRequestDTO) {
+    public ResponseEntity<String> createShortUrl(@Valid @RequestBody ShortUrlRequestDTO shortUrlRequestDTO) {
         String shortedUrl = shortUrlService.createShortUrl(shortUrlRequestDTO.originalUrl());
         return ResponseEntity.status(HttpStatus.CREATED).body(shortedUrl);
     }
